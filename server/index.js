73507047fde1dc1f46502332881fd952a4499193
Milton Lopez <miltonmarinol@gmail.com>
2022-09-16 22:21:02 -0500

    feat: ajustes.

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const session = require("express-session");
 const morgan = require("morgan");
 const routes = require("../routes/index.js");
 const server = express();
+const cors = require("cors");
 
 // server.name = "Authentication";
 
@@ -12,20 +13,7 @@ server.use(express.urlencoded({ extended: true, limit: "150mb" }));
 server.use(express.json({ limit: "50mb" }));
 server.use(cookieparser());
 server.use(morgan("dev"));
-server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS, PUT, DELETE"
-    );
-    next();
-});
-
+server.use(cors());
 server.use("/", routes);
 
 module.exports = server;
